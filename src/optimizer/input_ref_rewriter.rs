462db103569c2976ee9012820326a5d9b474b9f7
bit-aloo <sshourya17@gmail.com>
2025-12-27 22:31:27 +0530

    refactor InputRefRewriter to resolve aggregation expr

--- src/optimizer/input_ref_rewriter.rs
+++ src/optimizer/input_ref_rewriter.rs
@@ -2,7 +2,7 @@ use crate::{
     binder::expression::{BoundColumnRef, BoundExpr, BoundInputRef},
     optimizer::{
         expr_rewriter::ExprRewriter, logical_filter::LogicalFilter,
-        logical_project::LogicalProject, plan_rewriter::PlanRewriter,
+        logical_project::LogicalProject, plan_rewriter::PlanRewriter, LogicalAgg,
     },
 };
 use std::sync::Arc;
@@ -14,19 +14,52 @@ pub struct InputRefRewriter {
     bindings: Vec<BoundExpr>,
 }
 
-impl ExprRewriter for InputRefRewriter {
-    fn rewrite_column_ref(&self, expr: &mut BoundExpr) {
+impl InputRefRewriter {
+    fn rewrite_internal(&self, expr: &mut BoundExpr) {
+        // Find input expr in bindings
+        if let Some(idx) = self.bindings.iter().position(|e| *e == expr.clone()) {
+            *expr = BoundExpr::InputRef(BoundInputRef {
+                index: idx,
+                return_type: expr.return_type().unwrap(),
+            });
+            return;
+        }
+
+        // If not found in bindings, expand nested expr and then continuity rewrite_expr
         match expr {
-            BoundExpr::ColumnRef(_) => {
-                if let Some(idx) = self.bindings.iter().position(|e| *e == expr.clone()) {
-                    *expr = BoundExpr::InputRef(BoundInputRef {
-                        index: idx,
-                        return_type: expr.return_type().unwrap(),
-                    })
+            BoundExpr::BinaryOp(e) => {
+                self.rewrite_expr(&mut e.left);
+                self.rewrite_expr(&mut e.right);
+            }
+            BoundExpr::TypeCast(e) => self.rewrite_expr(&mut e.expr),
+            BoundExpr::AggFunc(e) => {
+                for arg in &mut e.exprs {
+                    self.rewrite_expr(arg);
                 }
             }
-            _ => unreachable!(),
+            _ => unreachable!(
+                "unexpected expr type {:?} for InputRefRewriter, binding: {:?}",
+                expr, self.bindings
+            ),
         }
+    }
+}
+
+impl ExprRewriter for InputRefRewriter {
+    fn rewrite_column_ref(&self, expr: &mut BoundExpr) {
+        self.rewrite_internal(expr);
+    }
+
+    fn rewrite_type_cast(&self, expr: &mut BoundExpr) {
+        self.rewrite_internal(expr);
+    }
+
+    fn rewrite_binary_op(&self, expr: &mut BoundExpr) {
+        self.rewrite_internal(expr);
+    }
+
+    fn rewrite_agg_func(&self, expr: &mut BoundExpr) {
+        self.rewrite_internal(expr);
     }
 }
 
@@ -74,6 +107,18 @@ impl PlanRewriter for InputRefRewriter {
         let new_plan = LogicalFilter::new(new_expr, new_child);
         Arc::new(new_plan)
     }
+
+    fn rewrite_logical_agg(&mut self, plan: &super::LogicalAgg) -> super::PlanRef {
+        let new_child = self.rewrite(plan.input());
+        let bindings = plan.agg_funcs();
+        let mut new_exprs = plan.agg_funcs();
+        for expr in &mut new_exprs {
+            self.rewrite_expr(expr);
+        }
+        self.bindings = bindings;
+        let new_plan = LogicalAgg::new(new_exprs, vec![], new_child);
+        Arc::new(new_plan)
+    }
 }
 
 #[cfg(test)]
@@ -84,12 +129,16 @@ mod input_ref_rewriter_test {
     use sqlparser::ast::BinaryOperator;
 
     use crate::{
-        binder::expression::{binary_op::BoundBinaryOp, BoundColumnRef, BoundExpr, BoundInputRef},
+        binder::expression::{
+            agg_func::{AggFunc, BoundAggFunc},
+            binary_op::BoundBinaryOp,
+            BoundColumnRef, BoundExpr, BoundInputRef,
+        },
         catalog::{ColumnCatalog, ColumnDesc},
         optimizer::{
             input_ref_rewriter::InputRefRewriter, logical_filter::LogicalFilter,
             logical_project::LogicalProject, logical_table_scan::LogicalTableScan,
-            plan_rewriter::PlanRewriter, PlanRef,
+            plan_rewriter::PlanRewriter, LogicalAgg, PlanRef,
         },
         types::ScalarValue,
     };
@@ -139,6 +188,18 @@ mod input_ref_rewriter_test {
         )
     }
 
+    fn build_logical_project_with_simple_agg(input: PlanRef) -> LogicalProject {
+        let expr = BoundExpr::AggFunc(BoundAggFunc {
+            func: AggFunc::Sum,
+            exprs: vec![BoundExpr::ColumnRef(BoundColumnRef {
+                column_catalog: build_test_column("c1".to_string()),
+            })],
+            return_type: DataType::Int32,
+        });
+        let simple_agg = LogicalAgg::new(vec![expr.clone()], vec![], input);
+        LogicalProject::new(vec![expr], Arc::new(simple_agg))
+    }
+
     #[test]
     fn test_rewrite_column_ref_to_input_ref() {
         let plan = build_local_table_scan();
@@ -167,5 +228,22 @@ mod input_ref_rewriter_test {
                 return_type: Some(DataType::Boolean),
             })
         );
+    }
+
+    #[test]
+    fn test_rewrite_simple_aggregation_column_ref_to_input_ref() {
+        let plan = build_local_table_scan();
+        let plan = build_logical_project_with_simple_agg(Arc::new(plan));
+
+        let mut rewriter = InputRefRewriter::default();
+        let new_plan = rewriter.rewrite(Arc::new(plan));
+
+        assert_eq!(
+            new_plan.as_logical_project().unwrap().exprs(),
+            vec![BoundExpr::InputRef(BoundInputRef {
+                index: 0,
+                return_type: DataType::Int32
+            })]
+        )
     }
 }
